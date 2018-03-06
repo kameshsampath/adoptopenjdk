2d9bdbebbc137d574ab98d25b6b90e09c9ec9aa4
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-03-06 20:35:02 +0530

    fixing goss setup issues

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -2,8 +2,13 @@
 set -e
 sudo npm -g install fish-pepper
 
-sudo curl -L  https://github.com/aelsabbahy/goss/releases/download/v0.3.5/goss-linux-amd64 -o /usr/local/bin/goss
-sudo chmod +rx /usr/local/bin/goss
+cd $HOMEDIR
+mkdir -p $HOMEDIR/bin
 
-sudo curl -L  https://raw.githubusercontent.com/aelsabbahy/goss/v0.3.5/extras/dgoss/dgoss -o /usr/local/bin/dgoss
-sudo chmod +rx /usr/local/bin/dgoss
+curl -L  https://github.com/aelsabbahy/goss/releases/download/v0.3.5/goss-linux-amd64 -o $HOMEDIR/bin/goss
+chmod +rx $HOMEDIR/bin/goss
+
+curl -L  https://raw.githubusercontent.com/aelsabbahy/goss/v0.3.5/extras/dgoss/dgoss -o $HOMEDIR/bin/bin/dgoss
+chmod +rx $HOMEDIR/bin/dgoss
+
+export PATH=$PATH:$HOMEDIR/bin
