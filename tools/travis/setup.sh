0c4537e1d2af057eeaae72323167735bc021bf6f
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-03-06 21:47:42 +0530

    sudoing chmod for goss/dgoss installs

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -4,7 +4,7 @@ set -e
 npm -g install fish-pepper
 
 sudo curl -L  https://github.com/aelsabbahy/goss/releases/download/v0.3.5/goss-linux-amd64 -o /usr/local/bin/goss
-chmod +rx /usr/local/bin/goss
+sudo chmod +rx /usr/local/bin/goss
 
 curl -L  https://raw.githubusercontent.com/aelsabbahy/goss/v0.3.5/extras/dgoss/dgoss -o  /usr/local/bin/dgoss
-chmod +rx /usr/local/bin/dgoss
+sudo chmod +rx /usr/local/bin/dgoss
