fe71e54c39944e463a236efbaf29f53e6e345ae4
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-03-06 21:26:52 +0530

    trying without sudo for docker

--- tools/travis/docker-push.sh
+++ tools/travis/docker-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eux
+set -e
 
 IMAGE_PREFIX=${1:-jdk}
 
@@ -7,7 +7,7 @@ if [[ ! -z ${DOCKER_USER} ]] && [[ ! -z ${DOCKER_PASSWORD} ]]; then
   docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
 fi
 
-sudo docker images | grep "$IMAGE_PREFIX" | awk '{n=$1":"$2; print n}' | while read -r i;
+docker images | grep "$IMAGE_PREFIX" | awk '{n=$1":"$2; print n}' | while read -r i;
 do
   docker push "${img}"
 done
