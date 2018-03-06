0cb32f5e04a528f99862df186872a55027f41409
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-03-06 21:04:55 +0530

    goss setup failed again fix

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 set -e
 
-SCRIPTDIR=$(cd $(dirname "$0") && pwd)
-ROOTDIR="$SCRIPTDIR/../.."
-HOMEDIR="$SCRIPTDIR/../../../"
 
 npm -g install fish-pepper
 
-cd $HOMEDIR
-mkdir -p $HOMEDIR/bin
+mkdir -p $PWD/bin
 
-curl -L  https://github.com/aelsabbahy/goss/releases/download/v0.3.5/goss-linux-amd64 -o $HOMEDIR/bin/goss
-chmod +rx $HOMEDIR/bin/goss
+curl -L  https://github.com/aelsabbahy/goss/releases/download/v0.3.5/goss-linux-amd64 -o $PWD/bin/goss
+chmod +rx $PWD/bin/goss
 
-curl -L  https://raw.githubusercontent.com/aelsabbahy/goss/v0.3.5/extras/dgoss/dgoss -o $HOMEDIR/bin/bin/dgoss
-chmod +rx $HOMEDIR/bin/dgoss
+curl -L  https://raw.githubusercontent.com/aelsabbahy/goss/v0.3.5/extras/dgoss/dgoss -o  $PWD/bin/dgoss
+chmod +rx $PWD/bin/dgoss
 
 export PATH=$PATH:$HOMEDIR/bin
